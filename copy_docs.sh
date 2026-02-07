57cb3e29563257447f0bac5e502475155395aa3d
Vishnu Sanal T <vishnusanalt@gmail.com>
2026-02-07 10:51:01 +0530

    update docs workflow after dokka migration
    Signed-off-by: Vishnu Sanal T <vishnusanalt@gmail.com>

--- copy_docs.sh
+++ copy_docs.sh
@@ -1,13 +1,13 @@
 KOTLIN_REPO="kotlin-repo"
 DOCUSAURUS_REPO="docusaurus-repo"
 
-cp -R $KOTLIN_REPO/build/dokka/htmlMultiModule/* $DOCUSAURUS_REPO/static/kdocs/
+cp -R $KOTLIN_REPO/build/dokka/html/* $DOCUSAURUS_REPO/static/kdocs/
 echo "KDocs copied to Docusaurus static/api directory"
 
 cp $KOTLIN_REPO/CHANGELOG.md                $DOCUSAURUS_REPO/changelog/0-changelog.md
 
-cp $KOTLIN_REPO/docs/contributing.md        $DOCUSAURUS_REPO/contributing/0-contributing.md
-cp $KOTLIN_REPO/docs/code-of-conduct.md     $DOCUSAURUS_REPO/contributing/1-code-of-conduct.md
+cp $KOTLIN_REPO/CONTRIBUTING.md             $DOCUSAURUS_REPO/contributing/0-contributing.md
+cp $KOTLIN_REPO/CODE-OF-CONDUCT.md          $DOCUSAURUS_REPO/contributing/1-code-of-conduct.md
 cp $KOTLIN_REPO/CODING-STYLE.md             $DOCUSAURUS_REPO/contributing/2-coding-style.md
 cp $KOTLIN_REPO/DEVELOPER-ENVIRONMENT.md    $DOCUSAURUS_REPO/contributing/3-developer-environment.md
 cp $KOTLIN_REPO/TESTING.md                  $DOCUSAURUS_REPO/contributing/4-testing.md
